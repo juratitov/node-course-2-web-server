a08be4a25e83307d9c6732b7ad12700ab74e3e1e
Jura <juratitov@gmail.com>
2017-11-26 01:50:37 +0200

    Lecture 45

--- server.js
+++ server.js
@@ -1,21 +1,60 @@
 const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err){
+            console.log('Unable to append to server.log');
+        }
+    });
+
+    next();
+});
+
+
+// app.use((req, res, next) => {
+//     res.render('maintenace');
+// });
+
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
     // res.send('<h1>Hello Express!</h1>');
-    res.send({
-        name: 'Jura',
-        links: [
-            'google',
-            'facebook'
-        ]
+    // res.send({
+    //     name: 'Jura',
+    //     links: [
+    //         'google',
+    //         'facebook'
+    //     ]
+    // });
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Wellcome to my website'
     });
 });
 
 
 app.get('/about', (req, res) => {
-    res.send('About page.');
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
 });
 
 app.get('/bad', (req, res) => {    
@@ -25,5 +64,6 @@ app.get('/bad', (req, res) => {
 });
 
 
-
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
